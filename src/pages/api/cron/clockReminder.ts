a1d7d8d3868f51d2ac0d35f248a0b45cf9ae2150
Patryk Scheffler <patryk.scheffler@gmail.com>
2023-03-23 09:23:20 +0100

    feat: clockIn, clockOut reminder

--- src/pages/api/cron/clockReminder.ts
+++ src/pages/api/cron/clockReminder.ts
@@ -1,10 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { add, sub } from "date-fns";
 import { type Channel } from "@mattermost/types/lib/channels";
 import { Client4 } from "@mattermost/client";
 
-import { prisma } from "~/server/db";
 import { env } from "~/env.mjs";
+import { getUsersToClockIn, getUsersToClockOut } from "~/server/timecard/clock";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // TODO: Check apiKey
@@ -15,52 +14,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return;
   }
 
-  const weekDay = (new Date().getDay() + 6) % 7;
-  // const currentDate = add(new Date(), { hours: 1 });
-  const currentDate = new Date(new Date().setUTCHours(7, 50, 0, 0));
-
   const botClient = new Client4();
   botClient.setUrl(env.MATTERMOST_URL);
   botClient.setToken(env.MATTERMOST_BOT_TOKEN);
 
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      accounts: {
-        select: {
-          provider: true,
-          providerAccountId: true,
-        },
-        where: {
-          provider: "mattermost",
-        }
-      },
-      availabilities: {
-        where: {
-          weekDay,
-        }
-      },
-    },
-    where: {
-      availabilities: {
-        every: {
-          start: {
-            lte: currentDate,
-            gt: sub(currentDate, { hours: 1 }),
-          }
-        }
-      },
-      timeCards: {
-        every: {
-          NOT: {
-            end: null
-          }
-        }
-      }
-    }
-  });
-
-  for (const user of users) {
+  const usersToClockIn = await getUsersToClockIn();
+  for (const user of usersToClockIn) {
     const mattermostUser = user.accounts.find((account) => account.provider === "mattermost");
     if (!mattermostUser) continue;
 
@@ -68,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const users = [
       env.MATTERMOST_BOT_ID,
       mattermostUser.providerAccountId,
-    ] as string[];
+    ];
 
     try {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -89,6 +48,47 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     }
                   },
                   name: "Clock In",
+                }
+              ]
+            }
+          ]
+        }
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const usersToClockOut = await getUsersToClockOut();
+  for (const user of usersToClockOut) {
+    const mattermostUser = user.accounts.find((account) => account.provider === "mattermost");
+    if (!mattermostUser) continue;
+
+    const message = `Hey! Don't forget to clock out!`;
+    const users = [
+      env.MATTERMOST_BOT_ID,
+      mattermostUser.providerAccountId,
+    ];
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const channel: Channel = await botClient.createDirectChannel(users);
+      await botClient.createPost({
+        channel_id: channel.id,
+        message,
+        props: {
+          attachments: [
+            {
+              // text: message,
+              actions: [
+                {
+                  integration: {
+                    url: `${env.NEXT_PUBLIC_WEBAPP_URL}/api/mattermost/clockAction`,
+                    context: {
+                      type: "out"
+                    }
+                  },
+                  name: "Clock Out",
                 }
               ]
             }
