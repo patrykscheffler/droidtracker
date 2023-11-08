129d0ca0ac63da8a3cc8e88f9daa78f3e78036d8
Patryk Scheffler <patryk.scheffler@gmail.com>
2023-11-08 13:12:35 +0100

    prepare Table component, upgrade packages

---
+++ src/components/timer/TimeLogs.tsx
@@ -0,0 +1,40 @@
+import { type ColumnDef } from "@tanstack/react-table";
+
+import { DataTable } from "~/components/ui/DataTable";
+
+export type Payment = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  email: string;
+};
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+  },
+];
+
+export default function TimeLogs() {
+  const data: Payment[] = [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
+
+  return (
+    <DataTable columns={columns} data={data} />
+  );
+}
